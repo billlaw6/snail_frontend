7845fd703d2260a4673968034afaf2dd409050bb
billlaw6 <liubin6@gmail.com>
2017-06-21 22:04:20 +0800

    2017-06-21 22:03 Dynamic Scroll components debugging

--- src/components/ScrollDiv.vue
+++ src/components/ScrollDiv.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="notice-list-box">
-    <ul class="notice-list" id="notice_list" :style="{width:ulWidth}">
-      <li>laskdfjalsdkfjla111111111111111111</li>
-      <li>laskdfjalsdkfjla222222222222222222</li>
-      <li>laskdfjalsdkfjla333333333333333333</li>
-    </ul>
+  <div id="scrollContent" class="notice-list-box">
+    <div v-for="message,index in messages" class="notice-list" id="notice_list" :style="{width:ulWidth}">
+      <h3>{{ message.title }}</h3>
+      <div>{{ message.content }}</div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +11,18 @@
   export default {
     data () {
       return {
-        ulWidth: '0px'
+        ulWidth: '0px',
+        messages: [{title: 'title1', content: 'content111111111111111111111'},
+          {title: 'title2', content: 'content222222222222222222222'},
+          {title: 'title3', content: 'content333333333333333333333'},
+          {title: 'title4', content: 'content444444444444444444444'}],
+        delay: 10,
+        amount: 1,
+        direction: 'up',
+        timeout: 1000,
+        autoScrollTimer: null,
+        scrollTimer: null,
+        scrollNum: 0
       }
     },
     methods: {
@@ -49,6 +59,49 @@
             }, 30)
           }
         }, 1000)
+      },
+
+      // 纵向滚动
+      scrollPlus: function () {
+        this.scrollContent.scrollTop += this.amount
+      },
+      start: function () {
+        clearTimeout(this.autoScrollTimer)
+        this.autoScrollTimer = setTimeout(this.GetFunction(this, 'autoScroll'), this.timeout)
+      },
+      autoScroll: function () {
+        let oneHeight = Math.round(parseInt(this.scrollContent.scrollHeight) / this.scrollNum)
+        let scrollTop = parseInt(this.scrollContent.scrollTop)
+        if (this.direction === 'up') {
+          if (scrollTop >= parseInt(this.scrollContent.scrollHeight - oneHeight)) {
+            this.scrollContent.scrollTop = 0
+            clearTimeout(this.autoScrollTimer)
+            this.autoScrollTimer = setTimeout(this.GetFunction(this, 'autoScroll'), this.timeout)
+            return
+          }
+          this.scrollContent.scrollTop += this.amount
+        } else {
+          if (parseInt(this.scrollContent.srollTop) <= 0) {
+            this.scrollContent.scrollTop = parseInt(this.scrollContent.scrollHeight) / 2
+          }
+          this.scrollContent.scrollTop -= this.amount
+        }
+
+        console.log(oneHeight)
+        if (parseInt(this.scrollContent.scrollTop) % oneHeight !== 0) {
+          this.scrollTimer = setTimeout(this.GetFunction(this, 'autoScroll'), this.delay)
+        } else {
+          this.autoScrollTimer = setTimeout(this.getFunction(this, 'autoScroll'), this.Timeout)
+        }
+      },
+      getFunction: function (variable, method) {
+        return function () {
+          variable[method]()
+        }
+      },
+      stop: function () {
+        clearTimeout(this.autoScrollTimer)
+        clearTimeout(this.scrollTimer)
       }
     },
     mounted: function () {
@@ -56,6 +109,14 @@
         this.createUlWidth()
         this.ulScroll()
       })
+      this.scrollContent = document.getElementById('scrollContent')
+      this.scrollNum = this.scrollContent.getElementsByTag('div').length
+      if (this.scrollNum > 1) {
+        this.start()
+      }
+    },
+    destroyed: function () {
+      this.stop()
     }
   }
 
